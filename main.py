845d6c86d472a36f357d9d022a029e80f86fcc98
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-04-23 15:05:50 +0300

    Добавлен фильтр активных пропусков

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import os
 
 import django
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcard = Passcard.objects.filter(is_active=True)
+
+    print('Количество пропусков:', Passcard.objects.count())
+    print('Активных пропусков:  ', len(passcard))
+
+
